87f4f3be188d0c110c5de7cdcc077b9f9b1d4c0b
Dendi Handian <handian.dendi@gmail.com>
2020-11-20 11:08:44 +0700

    product list owned by user

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::paginate(10);
+        $products = Product::where('user_id', Auth::user()->id)->paginate(10);
 
         return view('products.index', compact('products'));
     }
@@ -27,7 +27,7 @@ class ProductController extends Controller
         $params = $request->only(['title', 'description', 'price', 'stock']);
         $params['user_id'] = Auth::user()->id ?? null;
 
-        Product::create(params);
+        Product::create($params);
 
         $request->session()->flash('success', __('Product created'));
         return redirect()->route('products.index');
